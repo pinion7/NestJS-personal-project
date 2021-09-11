9ac1f5e477302595dca408c0f8adc3d76eb8d159
박민수 <pinion7rl@gmail.com>
2021-09-11 14:54:40 +0900

    typeorm 설치 및 entity, repository 생성

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Board } from './board.entity';
+import { BoardRepository } from './board.repository';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
 @Module({
+  imports: [
+    // TypeOrmModule.forFeature([Board])
+    TypeOrmModule.forFeature([BoardRepository])
+  ],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
