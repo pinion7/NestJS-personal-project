7fa79a1131df0915f5e08d22c9d98f31b8168afb
박민수 <pinion7rl@gmail.com>
2021-09-12 01:17:42 +0900

    typeorm 기반 게시판 crud 완성

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,45 +1,47 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardRepository } from './board.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './board.entity';
 
 @Injectable()
 export class BoardsService {
-  private boards: Board[] = [];
+  constructor(
+    @InjectRepository(BoardRepository)
+    private boardRepository: BoardRepository,
+  ) {}
 
-  getAllBoards(): Board[] {
-    return this.boards;
+  async getAllBoards(): Promise<Board[]> {
+    return this.boardRepository.find();
+  }
+  
+  createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+    return this.boardRepository.createBoard(createBoardDto);
   }
 
-  createBoard(createBoardDto: CreateBoardDto) {
-    const { title, description } = createBoardDto;
-    const board: Board = {
-      id: uuid(),
-      title,
-      description,
-      status: BoardStatus.PUBLIC,
-    };
-
-    this.boards.push(board);
-    return board;
-  }
-
-  getBoardById(id: string): Board {
-    const found = this.boards.find(board => board.id === id);
+  async getBoardById(id: number): Promise <Board> {
+    const found = await this.boardRepository.findOne(id);
     if (!found) {
       throw new NotFoundException(`id가 ${id}인 게시물을 찾을 수 없습니다.`);
     }
     return found;
   }
 
-  deleteBoard(id: string): void {
-    const found = this.getBoardById(id);
-    this.boards = this.boards.filter((board) => board.id !== found.id);
+  async deleteBoard(id: number): Promise<void> {
+    const result = await this.boardRepository.delete(id);
+    if(!result.affected) {
+      throw new NotFoundException(`id가 ${id}인 게시물을 찾을 수 없습니다.`);
+    }
+    // await this.getBoardById(id);
+    // await this.boardRepository.remove(id);
   }
 
-  updateBoardStatus(id: string, status: BoardStatus): Board {
-    const board = this.getBoardById(id);
+  async updateBoardStatus(id: number, status: BoardStatus): Promise<Board> {
+    const board = await this.getBoardById(id);
     board.status = status;
+    await board.save();
     return board;
   }
 }
